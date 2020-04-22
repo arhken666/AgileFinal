1bb493b28e582def8ea0e4b4cc07a541714061ef
xjtusezx <zx1584935@stu.xjtu.edu.cn>
2020-04-22 16:22:32 +0800

    Junit5 test finish

--- 后台/src/test/java/com/todolist/demo/repository/EventRepositoryTest.java
+++ 后台/src/test/java/com/todolist/demo/repository/EventRepositoryTest.java
@@ -14,5 +14,28 @@ class EventRepositoryTest {
         System.out.println(eventRepository.findAll());
     }
 
+    @Test
+    void save(){
+        Todoevent todoevent = new Todoevent();
+        todoevent.setName("Spring Boot");
+        todoevent.setComplete(0);
+        Todoevent to1 = eventRepository.save(todoevent);
+        System.out.println(to1);
+    }
+    @Test
+    void findById(){
+        Todoevent todoevent = eventRepository.findById(6).get();
+        System.out.println(todoevent);
+    }
     
+    @Test
+    void update(){
+        Todoevent todoevent = eventRepository.findById(2).get();
+        todoevent.setName("测试修改");
+        eventRepository.save(todoevent);
+    }
+    @Test
+    void deleteById(){
+        eventRepository.deleteById(1);
+    }
 }
