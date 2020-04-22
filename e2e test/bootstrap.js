fd837bae1003bbb7a86f8758224370e504ecb029
xjtusezx <zx1584935@stu.xjtu.edu.cn>
2020-04-22 17:18:53 +0800

    e2e pushed

---
+++ e2e test/bootstrap.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+const { expect } = require('chai');
+const _ = require('lodash');
+const globalVariables = _.pick(global, ['browser', 'expect']);
+
+// puppeteer配置
+const options = {
+  headless: false,
+  timeout: 10000,
+  executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+};
+
+// 启动浏览器
+before (async function () {
+  global.expect = expect;
+  global.browser = await puppeteer.launch(options);
+});
+
+// 测试之后运行，主要是用来关闭测试打开的浏览器
+after (function () {
+  browser.close();
+
+  global.browser = globalVariables.browser;
+  global.expect = globalVariables.expect;
+});
